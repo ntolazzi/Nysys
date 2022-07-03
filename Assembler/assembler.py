9371b01be3ad0e3a3f1c79402e649ac25ebe25b3
Nicolas Tolazzi <nicolas.tolazzi@norealemail.com>
2022-07-03 15:02:58 +0200

    Added labels and variables. First fully working version

--- Assembler/assembler.py
+++ Assembler/assembler.py
@@ -56,29 +56,29 @@ jump_binary_map = {
 }
 
 predefined_symbols = {
-    "R0": "0",
-    "R1": "1",
-    "R2": "2",
-    "R3": "3",
-    "R4": "4",
-    "R5": "5",
-    "R6": "6",
-    "R7": "7",
-    "R8": "8",
-    "R9": "9",
-    "R10": "10",
-    "R11": "11",
-    "R12": "12",
-    "R13": "13",
-    "R14": "14",
-    "R15": "15",
-    "SP": "0",
-    "LCL": "1",
-    "ARG": "2",
-    "THIS": "3",
-    "THAT": "4",
-    "SCREEN": "16384",
-    "KBD": "24576",
+    "R0": 0,
+    "R1": 1,
+    "R2": 2,
+    "R3": 3,
+    "R4": 4,
+    "R5": 5,
+    "R6": 6,
+    "R7": 7,
+    "R8": 8,
+    "R9": 9,
+    "R10": 10,
+    "R11": 11,
+    "R12": 12,
+    "R13": 13,
+    "R14": 14,
+    "R15": 15,
+    "SP": 0,
+    "LCL": 1,
+    "ARG": 2,
+    "THIS": 3,
+    "THAT": 4,
+    "SCREEN": 16384,
+    "KBD": 24576,
 
 }
 
@@ -105,16 +105,36 @@ def load_file(filename: str) -> str:
 
 
 def translate_c_instruction(inst: CInstruction) -> str:
-    print(inst)
+    # print(inst)
     return f"111{comp_binary_map[inst.comp]}{dest_binary_map[inst.dest]}{jump_binary_map[inst.jump]}"
 
 
-def translate_a_instruction(constant: str) -> str:
-    constant = int(constant)
+def translate_a_instruction(constant: int) -> str:
     if constant >= 2 ** 15:
         raise ValueError("Number outside of range [0,32767]")
     two_complement = format(constant, '015b')
     return f"0{two_complement}"
+
+
+class SymbolTable:
+    def __init__(self):
+        self.symbols = predefined_symbols
+        self.variable_counter = 16
+
+    def __contains__(self, item):
+        return item in self.symbols
+
+    def add(self, symbol, address):
+        self.symbols[symbol] = address
+        # print(f"Added label {symbol} to {address}")
+
+    def add_free(self, symbol):
+        self.symbols[symbol] = self.variable_counter
+        # print(f"Added variable {symbol} to {self.variable_counter}")
+        self.variable_counter += 1
+
+    def get(self, symbol):
+        return self.symbols[symbol]
 
 
 class Parser:
@@ -123,7 +143,9 @@ class Parser:
         self.file_content = file_content.splitlines()
         self.total_lines = len(self.file_content)
         self.current_line = None
+        self.current_inst_index = None
         self.current_inst = None
+        self.symbols = SymbolTable()
 
     def has_more_lines(self) -> bool:
         if self.current_line is None:
@@ -147,8 +169,6 @@ class Parser:
             return self.current_inst.strip("()")
         elif self.current_inst.startswith("@"):
             return self.current_inst.strip("@")
-        else:
-            raise ValueError("Does not start with '(' or '@'")
 
     def decode_current_c_inst(self) -> CInstruction:
         inst = self.current_inst
@@ -177,8 +197,8 @@ class Parser:
         if expression.startswith("//"):
             return ""
         if "//" in expression:
-            expression = expression.split("")[0]
-        expression.replace(" ", "")
+            expression = expression.split("//")[0]
+        expression = expression.replace(" ", "")
         return expression
 
     def advance(self):
@@ -196,19 +216,56 @@ class Parser:
                 is_valid_line = True
                 self.current_inst = candidate
 
+    def increment_inst_index(self):
+        if self.current_inst_index is None:
+            self.current_inst_index = 0
+        else:
+            self.current_inst_index += 1
+
+
 
 def assemble(parser: Parser) -> str:
     translated_output = ""
+
+    while parser.has_more_lines():
+        parser.advance()
+        inst_type = parser.get_current_inst_type()
+        if inst_type == InstType.L_Inst:
+            label = parser.get_current_inst_symbol()
+            if label not in parser.symbols:
+                parser.symbols.add(label, parser.current_inst_index + 1)
+            else:
+                raise ValueError(f"{parser.current_line}: {label} already defined")
+            continue
+        parser.increment_inst_index()
+
+    parser.current_line = None
+    parser.current_inst = None
+    parser.current_inst_index = None
+
     while parser.has_more_lines():
         parser.advance()
         inst_type = parser.get_current_inst_type()
         if inst_type == InstType.C_Inst:
             translated_inst = translate_c_instruction(parser.decode_current_c_inst())
         elif inst_type == InstType.A_Inst:
-            translated_inst = translate_a_instruction(parser.get_current_inst_symbol())
+            a_target = parser.get_current_inst_symbol()
+            try:
+                value = int(a_target)
+            except ValueError:
+                if a_target not in parser.symbols:
+                    parser.symbols.add_free(a_target)
+                value = parser.symbols.get(a_target)
+                # print(f"{a_target} = {value}")
+            translated_inst = translate_a_instruction(value)
+        elif inst_type == InstType.L_Inst:
+            continue
         else:
-            raise NotImplementedError("Type not implemented")
+            raise ValueError(f"{parser.current_line}: Could not decode line")
+
+        parser.increment_inst_index()
         translated_output += f"{translated_inst}\n"
+
     return translated_output[:-1]
 
 
